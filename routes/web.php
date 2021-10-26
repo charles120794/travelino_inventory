a73f3c9f158d17d25a5ca9ecfeb7b374acee0d75
Charles Dave Wong <wongcharlesdave@gmail.com>
2021-10-26 23:25:35 +0800

    Adding Artisan COnsole Command

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,30 @@
 <?php
 
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+});
+
+Route::get('/config-clear', function() {
+    Artisan::call('config:clear');
+});
+
+Route::get('/cache-clear', function() {
+    Artisan::call('cache:clear');
+});
+
+Route::get('/config-cache', function() {
+    Artisan::call('config:Cache');
+});
+
+Route::get('/storage-link', function() {
+    Artisan::call('storage:link');
+});
+
+
+Route::get('/system-down', function() {
+    Artisan::call('down');
+});
+
 
 Route::get('/categories', 'CategoryController@index');
 
